a73d2df3c14ed48415d7c5e889b12ae17cb5f1a1
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-03-20 21:58:28 +0700

    Refactor main.cpp to enhance functionality and add EEPROM backup; update WiFi credentials and expand translation map

--- ESP-32Translate/src/main.cpp
+++ ESP-32Translate/src/main.cpp
@@ -2,8 +2,9 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 #include <cstdlib>
-#include<string.h>
-#include<map>
+#include <EEPROM.h>
+#include <string.h>
+#include <map>
 
 using std::string;
 
@@ -12,59 +13,91 @@ using std::string;
 #define MQTT_USERNAME ""
 #define MQTT_PASSWORD ""
 #define MQTT_NAME "ESP_Client"
-#define RXD2 16 
+#define RXD2 16
 #define TXD2 17
+#define EEPROM_SIZE 1024
 
 WiFiClient client;
 PubSubClient mqtt(client);
-HardwareSerial mySerial(2); 
+HardwareSerial mySerial(2);
 DFRobotDFPlayerMini myDFPlayer;
 
-const char *ssid = "อิอิ";
-const char *pass = "44444444";
+const char *ssid = "Iphone";
+const char *pass = "tatty040347";
 bool feed = true;
 String Word[100] = {""};
+void backupEEPROM(String value, int length)
+{
+  EEPROM.begin(EEPROM_SIZE);
+  for (int i = 0; i < length; i++)
+  {
+    EEPROM.write(i, value[i]);
+  }
+  EEPROM.commit();
+}
 void print_word(int count)
 {
-  for(int i =0;i<count;i++)
+  for (int i = 0; i < count; i++)
   {
     Serial.println(Word[i]);
   }
 }
-void ShowMessage(String test)
+void OutController(String value)
 {
-  std::map<string,int> TransProtocals =
+  std::map<string, int> TransProtocals =
+      {
+          {"Hello", 1},
+          {"My", 2},
+          {"Name", 3},
+          {"Is", 4},
+          {"Chayathon", 5},
+          {"Rungrueang", 6},
+          {"Kasetsartuniversity", 7},
+          {"Goodbye", 8},
+          {"Thankyou", 9},
+          {"SeeYouLater", 10},
+          {"GoodMorning", 11},
+          {"GoodNight", 12},
+          {"GoodAfternoon", 13},
+          {"GoodEvening", 14},
+          {"HowAreYou", 15},
+          {"ImFine", 16},
+          {"NiceToMeetYou", 17},
+          {"Welcome", 18},
+          {"Sorry", 19},
+          {"ExcuseMe", 20},
+          {"Repeat", 999},
+      };
+      int number = value.charAt(0) - '0';
+      String reload = "";
+      int counting = 0;
+  if (value != "1Repeat")
   {
-    {"Hello",1},
-    {"My",2},
-    {"Name",3},
-    {"Is",4},
-    {"Chayathon",5},
-    {"Rungrueang",6},
-    {"Kasetsartuniversity",7},
-  };
-
-  int number = test.charAt(0) - '0';  
-  String reload = "";
-  int counting =0;
-  for (int i = 1; i <= test.length(); i++)
-  {
-    if (isUpperCase(test.charAt(i)) || i == test.length())
+    for (int i = 1; i <= value.length(); i++)
     {
-      if (!reload.isEmpty()) {
-        Word[counting++] = reload;
-        if (counting >= 100) break;  
+      if (isUpperCase(value.charAt(i)) || i == value.length())
+      {
+        if (!reload.isEmpty())
+        {
+          Word[counting++] = reload;
+          if (counting >= 100)
+            break;
+        }
+        reload = String(value.charAt(i));
       }
-      reload = String(test.charAt(i));
-    }
-    else
-    {
-      reload += test.charAt(i);
+      else
+      {
+        reload += value.charAt(i);
+      }
     }
   }
+  // // Back up value
+  // backupEEPROM(value, value.length());
+  // // backup
 
-  for (int i = 0; i < number ; i++)
+  for (int i = 0; i < number; i++)
   {
+
     if (TransProtocals.find(Word[i].c_str()) != TransProtocals.end())
     {
       int PointNumber = TransProtocals[Word[i].c_str()];
@@ -88,6 +121,21 @@ void ShowMessage(String test)
   print_word(counting);
 }
 
+void Showsound(int number)
+{
+  if (myDFPlayer.available())
+  {
+    myDFPlayer.play(number);
+    Serial.print("Playing: ");
+    Serial.println(number);
+    delay(1000);
+  }
+  else
+  {
+    Serial.print("Error: No sound file for ");
+    Serial.println(number);
+  }
+}
 void callback(char *topic, byte *payload, unsigned int length)
 {
   char msg[length + 1];
@@ -103,33 +151,33 @@ void callback(char *topic, byte *payload, unsigned int length)
   if (topic_str == "Translate/ESP32/Word")
   {
     feed = false;
-    ShowMessage(payload_str);
+    OutController(payload_str);
   }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  mySerial.begin(9600, SERIAL_8N1, RXD2, TXD2);  
+  mySerial.begin(9600, SERIAL_8N1, RXD2, TXD2);
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(100);
     Serial.print(".");
   }
-  if (!myDFPlayer.begin(mySerial)) {  
+  if (!myDFPlayer.begin(mySerial))
+  {
     Serial.println("DFPlayer Nah ready");
-    while (true);
+    while (true)
+      ;
     {
       Serial.print(".");
     }
   }
 
-
   mqtt.setServer(MQTT_SERVER, MQTT_PORT);
   mqtt.setCallback(callback);
   myDFPlayer.volume(30);
-
 }
 int newtime = 0;
 void loop()
